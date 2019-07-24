82dae1e83be923dfd4598870abc937e83f363dd0
Blu911 <krys.blu@gmail.com>
2019-07-24 11:36:03 +0200

    Refactor: add comments

--- src/main/java/com/blu911/herorestapi/hero/HeroRepository.java
+++ src/main/java/com/blu911/herorestapi/hero/HeroRepository.java
@@ -1,9 +1,10 @@
 package com.blu911.herorestapi.hero;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
+import java.util.List;
+
 public interface HeroRepository extends JpaRepository<Hero, Long> {
-    Hero findById(long id);
+    Hero findHeroById(Long id);
+    List<Hero> findAll();
 }
