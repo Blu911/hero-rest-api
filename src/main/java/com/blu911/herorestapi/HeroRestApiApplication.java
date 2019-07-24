fbd486bc542f1a2fe155ce577abda051cc2a1451
Blu911 <krys.blu@gmail.com>
2019-07-24 11:34:35 +0200

    Refactor: create Heroes moved to controller

--- src/main/java/com/blu911/herorestapi/HeroRestApiApplication.java
+++ src/main/java/com/blu911/herorestapi/HeroRestApiApplication.java
@@ -1,15 +1,7 @@
 package com.blu911.herorestapi;
 
-import com.blu911.herorestapi.hero.Hero;
-import com.blu911.herorestapi.hero.HeroGenerator;
-import com.blu911.herorestapi.hero.HeroRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import java.util.HashSet;
-import java.util.Set;
 
 @SpringBootApplication
 public class HeroRestApiApplication {
@@ -18,20 +10,4 @@ public class HeroRestApiApplication {
         SpringApplication.run(HeroRestApiApplication.class, args);
     }
 
-    /*This Bean is used to generate example heroes and insert them into the database
-    for testing phase
-    TODO: Remove this Bean after development completed
-     */
-    @Bean
-    CommandLineRunner init(HeroRepository heroRepository) {
-        return args -> {
-            HeroGenerator heroGenerator = new HeroGenerator();
-            Set<Hero> heroSet = new HashSet<>();
-
-            while (heroSet.size() < 100) {
-                heroSet.add(heroGenerator.generateHero());
-            }
-            heroRepository.saveAll(heroSet);
-        };
-    }
 }
--- src/main/java/com/blu911/herorestapi/hero/HeroController.java
+++ src/main/java/com/blu911/herorestapi/hero/HeroController.java
@@ -1,54 +1,66 @@
 package com.blu911.herorestapi.hero;
 
-import lombok.extern.slf4j.Slf4j;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.PostConstruct;
 import javax.validation.Valid;
-import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Set;
 
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
-@Slf4j
+@RequiredArgsConstructor
 public class HeroController {
 
-    private final HeroRepository heroRepository;
+    private final HeroService heroService;
 
-    public HeroController(HeroRepository heroRepository) {
-        this.heroRepository = heroRepository;
+    /*This method is used to generate example heroes and insert them into the database
+    for testing phase
+    TODO: Remove this method after development completed
+    */
+    @PostConstruct
+    public void generateFakeHeroes() {
+        HeroGenerator heroGenerator = new HeroGenerator();
+        Set<Hero> heroSet = new HashSet<>();
+
+        while (heroSet.size() < 10) {
+            heroSet.add(heroGenerator.generateHero());
+        }
+        heroService.saveAll(heroSet);
     }
 
     @RequestMapping(path = "/heroes", method = RequestMethod.GET, produces = "application/json")
     public ResponseEntity showAllHeroes() {
-        List<Hero> heroes = heroRepository.findAll();
-        List<Hero> heroesById = heroes.stream().sorted(Comparator.comparingLong(Hero::getId)).collect(Collectors.toList());
+        List<Hero> heroes = heroService.findAll();
+        List<Hero> heroesById = heroService.sortHeroesByIdASC(heroes);
         return ResponseEntity.ok(heroesById);
     }
 
     @RequestMapping(path = "/heroes/{id}", method = RequestMethod.GET, produces = "application/json")
-    public ResponseEntity showHero(@PathVariable long id) {
-        Hero hero = heroRepository.findById(id);
+    public ResponseEntity showHero(@PathVariable Long id) {
+        Hero hero = heroService.findHeroById(id);
         return ResponseEntity.ok(hero);
     }
 
     @RequestMapping(path = "/heroes", method = RequestMethod.PUT, produces = "application/json", consumes = "application/json")
     public ResponseEntity updateHero(@Valid @RequestBody Hero hero) {
-        Hero updatedHero = heroRepository.save(hero);
+        Hero updatedHero = heroService.save(hero);
         return ResponseEntity.ok(updatedHero);
     }
 
     @RequestMapping(path = "/heroes", method = RequestMethod.POST, produces = "application/json", consumes = "application/json")
     public ResponseEntity addHero(@Valid @RequestBody Hero hero) {
-        Hero addedHero = heroRepository.save(hero);
+        Hero addedHero = heroService.save(hero);
         return ResponseEntity.ok(addedHero);
     }
 
     @RequestMapping(path = "/heroes/{id}", method = RequestMethod.DELETE, produces = "application/json", consumes = "application/json")
-    public ResponseEntity deleteHero(@PathVariable long id) {
-        heroRepository.deleteById(id);
+    public ResponseEntity deleteHero(@PathVariable Long id) {
+        heroService.deleteById(id);
         return ResponseEntity.ok(id);
     }
 }
